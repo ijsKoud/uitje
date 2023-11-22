34aa7b4bd5b91a3a5d92867b379da7b91f490c24
Daan Klarenbeek <github@ijskoud.dev>
2023-11-22 20:15:03 +0100

    feat(Ui): add more components

--- packages/ui/avatar.tsx
+++ packages/ui/avatar.tsx
@@ -23,7 +23,7 @@ const AvatarFallback = React.forwardRef<
 >(({ className, ...props }, ref) => (
 	<AvatarPrimitive.Fallback
 		ref={ref}
-		className={cn("flex h-full w-full items-center justify-center rounded-full bg-zinc-100 dark:bg-zinc-800", className)}
+		className={cn("flex h-full w-full items-center justify-center rounded-full bg-muted", className)}
 		{...props}
 	/>
 ));
